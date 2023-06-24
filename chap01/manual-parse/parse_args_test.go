386b6a98e2c9fef045c63c456e0fbafca5fd0805
Lorena Holanda <lrnholanda@gmail.com>
2023-06-24 14:35:59 -0300

    test go fixed up

--- chap01/manual-parse/parse_args_test.go
+++ chap01/manual-parse/parse_args_test.go
@@ -5,26 +5,48 @@ import (
 	"testing"
 )
 
-type testConfig struct {
-	args []string
-	err  error
-	config
-}
-
 func TestParseArgs(t *testing.T) {
+	type testConfig struct {
+		args []string
+		err  error
+		config
+	}
+	tests := []testConfig{
+		{
+			args:   []string{"-h"},
+			err:    nil,
+			config: config{printUsage: true, numTimes: 0},
+		},
+		{
+			args:   []string{"10"},
+			err:    nil,
+			config: config{printUsage: false, numTimes: 10},
+		},
+		{
+			args:   []string{"abc"},
+			err:    errors.New("strconv.Atoi: parsing \"abc\": invalid syntax"),
+			config: config{printUsage: false, numTimes: 0},
+		},
+		{
+			args:   []string{"1", "foo"},
+			err:    errors.New("Invalid number of arguments"),
+			config: config{printUsage: false, numTimes: 0},
+		},
+	}
+
 	for _, tc := range tests {
 		c, err := parseArgs(tc.args)
-		if tc.result.err != nil && err.Error() != tc.result.err.Error() {
-			t.Fatalf("Expected error to be: %v, got: %v\n", tc.result.err, err)
+		if tc.err != nil && err.Error() != tc.err.Error() {
+			t.Fatalf("Expected error to be: %v, got: %v\n", tc.err, err)
 		}
-		if tc.result.err == nil && err != nil {
-			t.Errorf("Expected nil error, got: %v\n", err)
+		if tc.err == nil && err != nil {
+			t.Fatalf("Expected nil error, got: %v\n", err)
 		}
-		if c.printUsage != tc.result.printUsage {
-			t.Errorf("Expected printUsage to be: %v, got: %v\n", tc.result.printUsage, c.printUsage)
+		if c.printUsage != tc.printUsage {
+			t.Errorf("Expected printUsage to be: %v, got: %v\n", tc.printUsage, c.printUsage)
 		}
-		if c.numTimes != tc.result.numTimes {
-			t.Errorf("Expected numTimes to be: %v, got: %v\n", tc.result.numTimes, c.numTimes)
+		if c.numTimes != tc.numTimes {
+			t.Errorf("Expected numTimes to be: %v, got: %v\n", tc.numTimes, c.numTimes)
 		}
 	}
 }
